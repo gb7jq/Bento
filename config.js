3a2713bbed22f7cff0eeca3c4544e4d330627e35
gb7jq <158597517+gb7jq@users.noreply.github.com>
2024-02-02 23:39:10 +0700

    Update config.js
    changed name and weather

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Georgia',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'https://api.openweathermap.org/data/2.5/weather?lat={13.809490}&lon={100.537680}&appid={9f0f77a62f44778ba987d689bacd125a}', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '13.809490',
+	defaultLongitude: '100.537680',
 
 	// Autochange
 	autoChangeTheme: true,
